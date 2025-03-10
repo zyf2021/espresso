c14ab7dc8355031e3ccf7adf23abe33925aeb89a
zyf2021 <freelance2020@bk.ru>
2025-03-10 13:07:29 +0300

    решение

--- main.py
+++ main.py
@@ -1,50 +1,26 @@
 import sys
 import sqlite3
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QPushButton, QDialog
-from addEditCoffeeForm import AddEditCoffeeForm
-
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 
 class CoffeeApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("main.ui", self)
+        uic.loadUi("main.ui", self)  # Загружаем UI
         self.load_data()
 
-        # Кнопки
-        self.addButton.clicked.connect(self.add_coffee)
-        self.editButton.clicked.connect(self.edit_coffee)
-
     def load_data(self):
-        """Загрузка данных из базы в таблицу."""
         con = sqlite3.connect("coffee.sqlite")
         cur = con.cursor()
-        data = cur.execute("SELECT * FROM coffee").fetchall()
+        result = cur.execute("SELECT * FROM coffee").fetchall()
         con.close()
 
-        self.tableWidget.setRowCount(len(data))
-        self.tableWidget.setColumnCount(len(data[0]))
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
 
-        for i, row in enumerate(data):
-            for j, value in enumerate(row):
-                self.tableWidget.setItem(i, j, QTableWidgetItem(str(value)))
-
-    def add_coffee(self):
-        """Открытие формы добавления кофе."""
-        dialog = AddEditCoffeeForm(self)
-        if dialog.exec():
-            self.load_data()
-
-    def edit_coffee(self):
-        """Открытие формы редактирования выбранной записи."""
-        selected = self.tableWidget.currentRow()
-        if selected == -1:
-            return
-        coffee_id = self.tableWidget.item(selected, 0).text()
-        dialog = AddEditCoffeeForm(self, coffee_id)
-        if dialog.exec():
-            self.load_data()
-
+        for row_idx, row_data in enumerate(result):
+            for col_idx, col_data in enumerate(row_data):
+                self.tableWidget.setItem(row_idx, col_idx, QTableWidgetItem(str(col_data)))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
