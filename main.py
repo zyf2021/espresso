bbc1da08b581b67dd1bf8a880954401141803ca4
zyf2021 <freelance2020@bk.ru>
2025-03-17 12:34:35 +0300

    answer cappuccino

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
+
+# main.py
 import sys
 import sqlite3
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QPushButton, QDialog
+from addEditCoffeeForm import AddEditCoffeeForm
+
 
 class CoffeeApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("main.ui", self)  # Загружаем UI
+        uic.loadUi("main.ui", self)
         self.load_data()
+        self.addButton.clicked.connect(self.add_coffee)
+        self.editButton.clicked.connect(self.edit_coffee)
 
     def load_data(self):
         con = sqlite3.connect("coffee.sqlite")
         cur = con.cursor()
-        result = cur.execute("SELECT * FROM coffee").fetchall()
+        data = cur.execute("SELECT * FROM coffee").fetchall()
         con.close()
 
-        self.tableWidget.setRowCount(len(result))
-        self.tableWidget.setColumnCount(len(result[0]))
+        self.tableWidget.setRowCount(len(data))
+        self.tableWidget.setColumnCount(len(data[0]))
 
-        for row_idx, row_data in enumerate(result):
-            for col_idx, col_data in enumerate(row_data):
-                self.tableWidget.setItem(row_idx, col_idx, QTableWidgetItem(str(col_data)))
+        for i, row in enumerate(data):
+            for j, value in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(value)))
+
+    def add_coffee(self):
+        dialog = AddEditCoffeeForm(self)
+        if dialog.exec():
+            self.load_data()
+
+    def edit_coffee(self):
+        selected = self.tableWidget.currentRow()
+        if selected == -1:
+            return
+        coffee_id = self.tableWidget.item(selected, 0).text()
+        dialog = AddEditCoffeeForm(self, coffee_id)
+        if dialog.exec():
+            self.load_data()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
